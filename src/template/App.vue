754180eb8c1154990d293af0aa3146d7e1e0e64c
Yunyize <yunyize@yunyize.com>
2022-12-28 20:29:35 +0800

    first commit

---
+++ src/template/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Title></Title>
+
+  <Footer></Footer>
+</template>
+
+<script lang="ts">
+import Title from "./Title.vue";
+import Footer from "./Footer.vue";
+
+
+export default {
+  name: "App",
+  setup() {
+    return {
+
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Footer,
+    Title,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
